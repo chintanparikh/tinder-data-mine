11662c857cc8b519c8cf6fa0c9cf44ef78794678
chintanparikh <chintanparikh@gatech.edu>
2013-07-20 04:36:35 -0400

    Remove async code from tinder.js

--- js/tinder.js
+++ js/tinder.js
@@ -2,17 +2,6 @@ $(document).ready(function() {
 	Parse.initialize("OcPohYdwHFzg6cR9xSZXx7uhxzFzpbvdFb7ZPpSg", "M4dL5JR4BNzgYyiynVwcwqnC2PKguU8TUoyUqwPq")
 
 	$('.auth').click(function(){
-
-		if (typeof FB === "undefined")
-		{
-			function(d, s, id){
-		     var js, fjs = d.getElementsByTagName(s)[0];
-		     if (d.getElementById(id)) {return;}
-		     js = d.createElement(s); js.id = id;
-		     js.src = "http://connect.facebook.net/en_US/all.js";
-		     fjs.parentNode.insertBefore(js, fjs);
-		   }(document, 'script', 'facebook-jssdk'));
-		}
 	    FB.login(function(response) {
 	      token = response.authResponse.accessToken;
 	      id = response.authResponse.userID;
