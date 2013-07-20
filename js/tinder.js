8adb036d9f368fd6daa2c39f04844992ada3cbaa
chintanparikh <chintanparikh@gatech.edu>
2013-07-20 04:31:20 -0400

    Make sure FB is defined

--- js/tinder.js
+++ js/tinder.js
@@ -2,18 +2,29 @@ $(document).ready(function() {
 	Parse.initialize("OcPohYdwHFzg6cR9xSZXx7uhxzFzpbvdFb7ZPpSg", "M4dL5JR4BNzgYyiynVwcwqnC2PKguU8TUoyUqwPq")
 
 	$('.auth').click(function(){
-    FB.login(function(response) {
-      token = response.authResponse.accessToken;
-      id = response.authResponse.userID;
-      if (token && id) {
-        console.log("Facebook token: " + token);
-        console.log("Facebook ID: " + id);
-        auth(token);
-        $('.auth').remove();
-      } else {
-        console.log('User cancelled login or did not fully authorize.');
-      }
-    }, {});
+
+		if (typeof FB === "undefined")
+		{
+			function(d, s, id){
+		     var js, fjs = d.getElementsByTagName(s)[0];
+		     if (d.getElementById(id)) {return;}
+		     js = d.createElement(s); js.id = id;
+		     js.src = "http://connect.facebook.net/en_US/all.js";
+		     fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
+		}
+	    FB.login(function(response) {
+	      token = response.authResponse.accessToken;
+	      id = response.authResponse.userID;
+	      if (token && id) {
+	        console.log("Facebook token: " + token);
+	        console.log("Facebook ID: " + id);
+	        auth(token);
+	        $('.auth').remove();
+	      } else {
+	        console.log('User cancelled login or did not fully authorize.');
+	      }
+	    }, {});
   });
 });
 
